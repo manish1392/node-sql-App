8e4f565a13de53276770a5839db263d2c3a45082
Manish Arora <manish.arora1392@gmail.com>
2019-01-06 18:20:16 -0800

    node-sql-App

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is up at port ${port}`);
+});
